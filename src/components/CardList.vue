8d64f94ff45f9762bb583731da156de1ad3795b7
Sabyrka <heysabyrka@gmail.com>
2026-04-21 03:32:14 +0500

    feat: fetch items from API and update CardList component

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,73 +1,10 @@
 <script setup>
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
+import axios from 'axios'
+
 import CardItem from './CardItem.vue'
 
-const items = ref([
-  {
-    id: 1,
-    title: 'Мужские Кроссовки Nike Blazer Mid Suede',
-    price: 12990,
-    imageUrl: '/sneakers/sneakers-1.jpg',
-    isFavorite: false,
-    isAdded: false,
-  },
-  {
-    id: 2,
-    title: 'Мужские Кроссовки Nike Air Max 270',
-    price: 15600,
-    imageUrl: '/sneakers/sneakers-2.jpg',
-    isFavorite: true,
-    isAdded: false,
-  },
-  {
-    id: 3,
-    title: 'Мужские Кроссовки Nike Air Force 1',
-    price: 13500,
-    imageUrl: '/sneakers/sneakers-3.jpg',
-    isFavorite: false,
-    isAdded: true,
-  },
-  {
-    id: 4,
-    title: 'Кроссовки Puma X Ray 2 Square',
-    price: 9800,
-    imageUrl: '/sneakers/sneakers-4.jpg',
-    isFavorite: false,
-    isAdded: false,
-  },
-  {
-    id: 5,
-    title: 'Кроссовки Under Armour Curry 8',
-    price: 17400,
-    imageUrl: '/sneakers/sneakers-5.jpg',
-    isFavorite: false,
-    isAdded: false,
-  },
-  {
-    id: 6,
-    title: 'Кроссовки Nike Kyrie 7',
-    price: 11200,
-    imageUrl: '/sneakers/sneakers-6.jpg',
-    isFavorite: true,
-    isAdded: true,
-  },
-  {
-    id: 7,
-    title: 'Кроссовки Jordan Air Jordan 11',
-    price: 10700,
-    imageUrl: '/sneakers/sneakers-7.jpg',
-    isFavorite: false,
-    isAdded: false,
-  },
-  {
-    id: 8,
-    title: 'Кроссовки Nike LeBron 18',
-    price: 16400,
-    imageUrl: '/sneakers/sneakers-8.jpg',
-    isFavorite: false,
-    isAdded: false,
-  },
-])
+const items = ref([])
 
 const toggleFavorite = (item) => {
   item.isFavorite = !item.isFavorite
@@ -76,6 +13,15 @@ const toggleFavorite = (item) => {
 const toggleAdd = (item) => {
   item.isAdded = !item.isAdded
 }
+
+onMounted(async () => {
+  try {
+    const { data } = await axios.get('https://e5dc569b98392dff.mokky.dev/items')
+    items.value = data
+  } catch (error) {
+    console.error('Ошибка при загрузке данных:', error)
+  }
+})
 </script>
 
 <template>
