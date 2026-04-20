36d98ff94df7b51ba9d0449931e872782182af5a
Sabyrka <heysabyrka@gmail.com>
2026-04-21 02:53:09 +0500

    feat: style filter controls and implement reactive select state

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup>
+import { ref } from 'vue'
 // import AppDrawer from './components/Cart/AppDrawer.vue'
 import AppHeader from './components/AppHeader.vue'
 import CardList from './components/CardList.vue'
+
+const sortBy = ref('')
 </script>
 
 <template>
@@ -11,7 +14,33 @@ import CardList from './components/CardList.vue'
     <AppHeader />
 
     <section class="p-10">
-      <h2 class="text-3xl font-bold mb-10">Все кроссовки</h2>
+      <div class="flex justify-between items-center mb-10">
+        <h2 class="text-3xl font-bold">Все кроссовки</h2>
+
+        <div class="flex gap-4">
+          <select
+            v-model="sortBy"
+            :class="[
+              'py-2 px-3 border border-slate-200 rounded-md outline-none bg-white transition focus:border-gray-400',
+              sortBy ? 'text-black' : 'text-slate-400',
+            ]"
+          >
+            <option value="" disabled>Сортировка</option>
+            <option value="name" class="text-black">По названию</option>
+            <option value="price_asc" class="text-black">По цене (дешевые)</option>
+            <option value="price_desc" class="text-black">По цене (дорогие)</option>
+          </select>
+
+          <div class="relative text-slate-400 focus-within:text-gray-600">
+            <img class="absolute left-4 top-3 opacity-30" src="/search.svg" alt="Search" />
+            <input
+              type="text"
+              placeholder="Поиск..."
+              class="border border-slate-200 rounded-md py-2 pl-11 pr-4 outline-none text-black placeholder:text-slate-400 focus:border-gray-400 transition"
+            />
+          </div>
+        </div>
+      </div>
 
       <CardList />
     </section>
