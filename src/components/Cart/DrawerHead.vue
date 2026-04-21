240115a87efa53c7796aa7ebfff19bcea7451c8d
Sabyrka <heysabyrka@gmail.com>
2026-04-21 19:23:14 +0500

    refactor: add Icon prefix to icon components

--- src/components/Cart/DrawerHead.vue
+++ src/components/Cart/DrawerHead.vue
@@ -1,11 +1,11 @@
 <script setup>
-import ArrowRight from '../icons/ArrowRight.vue'
+import IconArrowRight from '../icons/IconArrowRight.vue'
 </script>
 
 <template>
   <header class="flex items-center gap-4 mb-8">
     <button class="group cursor-pointer flex items-center justify-center transition">
-      <ArrowRight />
+      <IconArrowRight />
     </button>
     <h2 class="text-2xl font-bold">Корзина</h2>
   </header>
